e0d01ca3d77068d804390c820df6c20eff776a56
CCColda <cccolda@gmail.com>
2023-12-11 12:48:02 +0100

    HTTP, HTTPResponse documentation

--- ekonyv/src/network/http.h
+++ ekonyv/src/network/http.h
@@ -1,12 +1,17 @@
+/**
+ * @file
+ * @brief Defines types for parsing HTTP packets.
+ */
+
 #ifndef EKONYV_HTTP_H
 #define EKONYV_HTTP_H
 
 #include "../config.h"
 
 #include <Arduino.h>
-#include <Ethernet.h>
 
 namespace HTTP {
+//! @brief Enumerates HTTP request methods.
 enum Method : uint8_t {
 	GET,
 	PUT,
@@ -17,6 +22,7 @@ enum Method : uint8_t {
 	m_unknown
 };
 
+//! @brief Enumerates HTTP client request headers.
 enum ClientHeader : uint8_t {
 	AUTHORIZATION,
 	ACCEPT,
@@ -25,6 +31,7 @@ enum ClientHeader : uint8_t {
 	ch_unknown
 };
 
+//! @brief Enumerates HTTP server response headers.
 enum ServerHeader : uint8_t {
 	CONTENT_TYPE,
 	CONNECTION,
@@ -34,6 +41,7 @@ enum ServerHeader : uint8_t {
 	sh_unknown
 };
 
+//! @brief Enumerates HTTP parsing statuses.
 enum ParseResult : uint8_t {
 	FAIL,
 	SUCCESS,
@@ -58,8 +66,13 @@ struct HeaderPair {
 using ClientHeaderPair = HeaderPair<ClientHeader>;
 using ServerHeaderPair = HeaderPair<ServerHeader>;
 
+//! @brief Holds the string representations of HTTP methods
 extern const char *METHOD_MAP[Method::m_size];
+
+//! @brief Holds the string representations of HTTP client request headers
 extern const char *CLIENT_HEADERS[ClientHeader::ch_size];
+
+//! @brief Holds the string representations of HTTP server response headers
 extern const char *SERVER_HEADERS[ServerHeader::sh_size];
 
 } // namespace HTTP
--- ekonyv/src/network/http_response.h
+++ ekonyv/src/network/http_response.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief Contains definitions of fixed HTTP response messages.
+ */
+
 #ifndef EKONYV_HTTP_RESPONSE_H
 #define EKONYV_HTTP_RESPONSE_H
 
@@ -10,46 +15,46 @@ struct StaticHTMLResponse {
 	const char *description;
 };
 
-const StaticHTMLResponse HTML_BAD_REQUEST = {
+constexpr static const StaticHTMLResponse HTML_BAD_REQUEST = {
     400,
     "Bad Request",
     "400 - Bad Request",
     "The request is malformed."};
 
-const StaticHTMLResponse HTML_NOT_FOUND = {
+constexpr static const StaticHTMLResponse HTML_NOT_FOUND = {
     404,
     "Not Found",
     "404 - Not Found",
     "The resource you requested does not seem to exist."};
 
-const StaticHTMLResponse HTML_UNAUTHORIZED = {
+constexpr static const StaticHTMLResponse HTML_UNAUTHORIZED = {
     401,
     "Unauthorized",
     "401 - Unauthorized",
     "Invalid credentials."};
 
-const StaticHTMLResponse HTML_NOT_IMPLEMENTED = {
+constexpr static const StaticHTMLResponse HTML_NOT_IMPLEMENTED = {
     501,
     "Unimplemented",
     "501 - Unimplemented",
     "The endpoint you provided was registered but not implemented."};
 
-const StaticHTMLResponse HTML_INVALID_BOOK = HTTPResponse::StaticHTMLResponse{
+constexpr static const StaticHTMLResponse HTML_INVALID_BOOK = HTTPResponse::StaticHTMLResponse{
     400, "Bad Request",
     "400 - Bad Request",
     "The book with the given ID doesn't exist."};
 
-const StaticHTMLResponse HTML_BOOK_WRITE_PROTECTED = HTTPResponse::StaticHTMLResponse{
+constexpr static const StaticHTMLResponse HTML_BOOK_WRITE_PROTECTED = HTTPResponse::StaticHTMLResponse{
     400, "Bad Request",
     "400 - Bad Request",
     "The book must be edited by its uploader."};
 
-const StaticHTMLResponse HTML_INVALID_USER = HTTPResponse::StaticHTMLResponse{
+constexpr static const StaticHTMLResponse HTML_INVALID_USER = HTTPResponse::StaticHTMLResponse{
     400, "Bad Request",
     "400 - Bad Request",
     "The user with the given ID doesn't exist."};
 
-const StaticHTMLResponse HTML_INVALID_STORAGE = HTTPResponse::StaticHTMLResponse{
+constexpr static const StaticHTMLResponse HTML_INVALID_STORAGE = HTTPResponse::StaticHTMLResponse{
     400, "Bad Request",
     "400 - Bad Request",
     "The storage place with the given ID doesn't exist."};
